d47f64119b426a43a9e2d425b28788bcd9767a21
freecontent <84922944+freecontent@users.noreply.github.com>
2021-11-26 18:37:32 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -1,12 +1,15 @@
 # This file is a part of TG-FileStreamBot
 # Coding : Jyothis Jayanth [@EverythingSuckz]
 
+import time
+
 from pyrogram import filters, emoji
 from WebStreamer.bot import StreamBot
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 @StreamBot.on_message(filters.command(['start', 'help']))
 async def start(_, m: Message):
+    print("uniq timestamp=====", int(time.time()))
     await m.reply(f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.',
                   reply_markup=InlineKeyboardMarkup(
                       [[
